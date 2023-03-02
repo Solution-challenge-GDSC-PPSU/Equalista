15b5a5857c6f9359d7b379b202798110e3c959e1
theshubh007 <thekingshubh07@gmail.com>
2023-03-02 16:04:20 +0530

    flow

--- lib/SignupFiles/loginpage.dart
+++ lib/SignupFiles/loginpage.dart
@@ -1,5 +1,7 @@
 import 'package:equalista/Globalservices/firebase_helper.dart';
+import 'package:equalista/SignupFiles/role_selectpage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Loginpage extends StatefulWidget {
   const Loginpage({Key? key}) : super(key: key);
@@ -31,7 +33,11 @@ class _LoginpageState extends State<Loginpage> {
                   // backgroundColor: Colors.lightBlue.shade100,
                 ),
                 onPressed: () {
-                  FirebaseHelper.signInWithGoogle();
+                     DialogBuilder(context).showLoadingIndicator();
+                  FirebaseHelper.signInWithGoogle().then((value) {
+                     DialogBuilder(context).hideOpenDialog();
+                    Get.to(() => Role_Selectpage());
+                  });
                 },
                 child: Container(
                   width: width * 0.70,
@@ -50,5 +56,83 @@ class _LoginpageState extends State<Loginpage> {
         ),
       ),
     ));
+  }
+}
+
+
+class DialogBuilder {
+  DialogBuilder(this.context);
+
+  final BuildContext context;
+
+  void showLoadingIndicator() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return WillPopScope(
+            onWillPop: () async => false,
+            child: const AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0))),
+              backgroundColor: Colors.black87,
+              content: LoadingIndicator(text: 'processing'),
+            ));
+      },
+    );
+  }
+
+  void hideOpenDialog() {
+    Navigator.of(context).pop();
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({this.text = ''});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    var displayedText = text;
+
+    return Container(
+        padding: const EdgeInsets.all(16),
+        color: Colors.black87,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _getLoadingIndicator(),
+              _getHeading(context),
+              _getText(displayedText)
+            ]));
+  }
+
+  Padding _getLoadingIndicator() {
+    return const Padding(
+        padding: EdgeInsets.only(bottom: 16),
+        child: SizedBox(
+            width: 32,
+            height: 32,
+            child: CircularProgressIndicator(strokeWidth: 3)));
+  }
+
+  Widget _getHeading(context) {
+    return const Padding(
+        padding: EdgeInsets.only(bottom: 4),
+        child: Text(
+          'Please wait …',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+          textAlign: TextAlign.center,
+        ));
+  }
+
+  Text _getText(String displayedText) {
+    return Text(
+      displayedText,
+      style: const TextStyle(color: Colors.white, fontSize: 14),
+      textAlign: TextAlign.center,
+    );
   }
 }
