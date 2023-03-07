e73ed03c43d06888bddf8fdf07958826bc4058f8
theshubh007 <thekingshubh07@gmail.com>
2023-03-07 09:23:44 +0530

    nav

--- lib/ConsulatantFiles/c_navbar.dart
+++ lib/ConsulatantFiles/c_navbar.dart
@@ -1,3 +1,5 @@
+import 'package:equalista/ConsulatantFiles/c_homepage.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -9,8 +11,35 @@ class C_navbar extends StatefulWidget {
 }
 
 class _C_navbarState extends State<C_navbar> {
+   List pages = [
+    const C_homepage(),
+    const C_homepage(),
+  ];
+  int currindex = 0;
+  void ontap(int index) {
+    setState(() {
+      currindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+   return Scaffold(
+      body: pages[currindex],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.teal,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+          showSelectedLabels: true,
+          onTap: ontap,
+          currentIndex: currindex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), label: "Profile"),
+         
+          ]),
+    );
   }
 }
--- lib/UserFiles/u_navbar.dart
+++ lib/UserFiles/u_navbar.dart
@@ -1,3 +1,5 @@
+import 'package:equalista/UserFiles/u_homepage.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -9,8 +11,33 @@ class U_navbar extends StatefulWidget {
 }
 
 class _U_navbarState extends State<U_navbar> {
+  List pages = [
+    const U_homepage(),
+    const U_homepage(),
+  ];
+  int currindex = 0;
+  void ontap(int index) {
+    setState(() {
+      currindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: pages[currindex],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.teal,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+          showSelectedLabels: true,
+          onTap: ontap,
+          currentIndex: currindex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ]),
+    );
   }
 }
