fb1ce07fb86d8634cd9dcb973fff986c77d9694e
theshubh007 <thekingshubh07@gmail.com>
2023-03-10 11:26:53 +0530

    drawer

--- lib/ConsulatantFiles/c_drawer.dart
+++ lib/ConsulatantFiles/c_drawer.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+import '../OnboardFiles/onboard._page.dart';
 
 class C_drawer extends StatefulWidget {
   const C_drawer({super.key});
@@ -11,6 +16,37 @@ class C_drawer extends StatefulWidget {
 class _C_drawerState extends State<C_drawer> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: Container(
+        color: Colors.white,
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              child: Text("Equalista"),
+            ),
+            ListTile(
+              title: const Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Logout"),
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                
+                Get.offAll(() => const Onboardpage());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/UserFiles/u_drawer.dart
+++ lib/UserFiles/u_drawer.dart
@@ -1,5 +1,9 @@
+import 'package:equalista/OnboardFiles/onboard._page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 
 class U_drawer extends StatefulWidget {
   const U_drawer({super.key});
@@ -11,6 +15,36 @@ class U_drawer extends StatefulWidget {
 class _U_drawerState extends State<U_drawer> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: Container(
+        color: Colors.white,
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              child: Text("Equalista"),
+            ),
+            ListTile(
+              title: const Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Logout"),
+              onTap: ()async {
+                await FirebaseAuth.instance.signOut();
+                Get.offAll(() => const Onboardpage());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
